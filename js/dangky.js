dea1724e5403c044b7ba1c281f3dc8a91ad8ddd9
buikimquangloc <118284013+buikimquangloc@users.noreply.github.com>
2022-12-04 17:06:54 +0700

    update dang ky

--- js/dangky.js
+++ js/dangky.js
@@ -45,3 +45,40 @@ function dangky(e){
     alert("Chúc mừng bạn đã đăng ký thành công!!");
     return true;
 }
+
+function dangnhap(e){ 
+    event.preventDefault();
+    var hoten = document.getElementById("hoten").value;
+    var email = document.getElementById("email").value;
+    var sdt = document.getElementById("sdt").value;
+    var mk = document.getElementById("mk").value;
+    var user = localStorage.getItem(hoten);
+    var data = JSON.parse(user);
+
+    var mail = /^[a-zA-Z0-9.-]+\[a-zA-Z]{2,4}$/;
+
+    if( mail.test(e.emai.value) == false){
+        alert("Mời bạn nhập email hợp lệ!");
+        e.email.focus();
+        return false;
+    }
+
+    if( e.pws.value.length < 8){
+        alert("Mật khẩu phải từ 8 ký tự.");
+        e.pws.focus();
+        return false;
+    }
+
+    alert("Đăng nhập thành công!");
+    return true;
+
+    // if(user == null){
+    //     alert("Ban can nhap ten va mat khau");
+    // } else if (hoten == data.hoten && email == data.email && sdt == data.sdt && mk == data.mk){
+    //     alert("Dang nhap thanh cong");
+    //     window.location.href = "/html/index.html"
+    // }
+    // else{
+    //     alert("Dang nhap khong thanh cong")
+    // }
+}
