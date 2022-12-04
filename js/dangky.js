a16d38f73efc8a49d7105f5b95d29b504bdc8e37
buikimquangloc <118284013+buikimquangloc@users.noreply.github.com>
2022-12-04 16:51:54 +0700

    update dang ky

--- js/dangky.js
+++ js/dangky.js
@@ -11,7 +11,37 @@ function dangky(e){
         sdt : sdt,
         mk : mk,
     }
+
     var json = JSON.stringify(user);
     localStorage.setItem(hoten,json);
     alert("dang ky thanh cong");
+
+    var mail = /^[a-zA-Z0-9.-]+\[a-zA-Z]{2,4}$/;
+
+    if(mail.test(e.email.value) == false){
+        alert("Bạn cần nhập Email hợp lệ.");
+        e.email.focus();
+        return false;
+    }
+
+    if(e.pws.value.length<8){
+        alert("Mật khẩu tối thiểu cần 8 ký tự!");
+        e.pws.focus();
+        return false;
+    }
+
+    if(e.pws2.value.length<8){
+        alert("Mật khẩu tối thiểu cần 8 ký tự!");
+        e.pws2.focus();
+        return false;
+    }
+
+    if(e.pws.value.length != e.pws2.value.length){
+        alert("Bạn cần nhập đúng mật khẩu nhập lại.");
+        e.pws.focus();
+        return false;
+    }
+
+    alert("Chúc mừng bạn đã đăng ký thành công!!");
+    return true;
 }
