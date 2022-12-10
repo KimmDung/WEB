182177ed8de6e8b6c76c7c3b243381f6ebe0895f
nguyenkhoa02 <94236317+nguyenkhoa02@users.noreply.github.com>
2022-12-10 22:06:24 +0700

    modified:   html/dangnhap.html 	modified:   html/sanphamchitiet.html 	modified:   js/dangky.js 	new file:   js/dangnhap.js

--- js/dangky.js
+++ js/dangky.js
@@ -1,44 +1,46 @@
 function dangky(e){
     // event.preventDefault();
-    var hoten = document.getElementById("hoten").value;
-    var email = document.getElementById("email").value;
-    var sdt = document.getElementById("sdt").value;
-    var mk = document.getElementById("mk").value;
+    var hoten = document.getElementById("hoten");
+    var email = document.getElementById("email");
+    var sdt = document.getElementById("sdt");
+    var mk = document.getElementById("mk");
+    var mk2 = document.getElementById("xacnhan");
+
 
     var user  = {
-        hoten : hoten,
-        emai : email,
-        sdt : sdt,
-        mk : mk,
+        hoten : hoten.value,
+        email : email.value,
+        sdt : sdt.value,
+        mk : mk.value,
     }
 
     var json = JSON.stringify(user);
-    localStorage.setItem(hoten,json);
+    localStorage.setItem('user',json);
     // alert("dang ky thanh cong");
 
-    var mail = /^[a-zA-Z0-9.-]+\[a-zA-Z]{2,4}$/;
+    var mail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 
-    if(mail.test(e.email.value) == false){
+    if(mail.test(email.value) == false){
         alert("Bạn cần nhập Email hợp lệ.");
-        e.email.focus();
+        email.focus();
         return false;
     }
 
-    if(e.pws.value.length<8){
+    if(mk.value.length<8){
         alert("Mật khẩu tối thiểu cần 8 ký tự!");
-        e.pws.focus();
+        mk.focus();
         return false;
     }
 
-    if(e.pws2.value.length<8){
+    if(mk2.value.length<8){
         alert("Mật khẩu tối thiểu cần 8 ký tự!");
-        e.pws2.focus();
+        mk2.focus();
         return false;
     }
 
-    if(e.pws.value.length != e.pws2.value.length){
+    if(mk.value.length != mk2.value.length){
         alert("Bạn cần nhập đúng mật khẩu nhập lại.");
-        e.pws.focus();
+        mk2.focus();
         return false;
     }
 
@@ -46,39 +48,3 @@ function dangky(e){
     return true;
 }
 
-function dangnhap(e){ 
-    event.preventDefault();
-    var hoten = document.getElementById("hoten").value;
-    var email = document.getElementById("email").value;
-    var sdt = document.getElementById("sdt").value;
-    var mk = document.getElementById("mk").value;
-    var user = localStorage.getItem(hoten);
-    var data = JSON.parse(user);
-
-    var mail = /^[a-zA-Z0-9.-]+\[a-zA-Z]{2,4}$/;
-
-    if( mail.test(e.emai.value) == false){
-        alert("Mời bạn nhập email hợp lệ!");
-        e.email.focus();
-        return false;
-    }
-
-    if( e.pws.value.length < 8){
-        alert("Mật khẩu phải từ 8 ký tự.");
-        e.pws.focus();
-        return false;
-    }
-
-    alert("Đăng nhập thành công!");
-    return true;
-
-    // if(user == null){
-    //     alert("Ban can nhap ten va mat khau");
-    // } else if (hoten == data.hoten && email == data.email && sdt == data.sdt && mk == data.mk){
-    //     alert("Dang nhap thanh cong");
-    //     window.location.href = "/html/index.html"
-    // }
-    // else{
-    //     alert("Dang nhap khong thanh cong")
-    // }
-}
